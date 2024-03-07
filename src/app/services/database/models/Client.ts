0cd0d42639ff03e36434ecf49531a31580607130
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2024-03-07 11:40:01 +0100

    #22 Database models created User/client/album/image

---
+++ src/app/services/database/models/Client.ts
@@ -0,0 +1,51 @@
+import { TClient } from "@/app/core/types";
+import {Schema,model, models} from "mongoose";
+import  {v4 as uuidv4}  from "uuid"; 
+
+
+
+const clientSchema = new Schema<TClient>({
+    clientId: {
+    type: String,
+    default: uuidv4(),
+    required: true,
+  },
+    clientName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+    photographerId: {
+        type: String,
+        required: true,
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    Albums: [{
+        type: String,
+        required: true,
+    }],
+    contact: {
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+    },
+    
+});
+
+const Client = models.Client || model('Client', clientSchema);
+
+
+
+export default Client;
