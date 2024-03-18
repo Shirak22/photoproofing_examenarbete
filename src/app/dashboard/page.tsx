f9401d519df926c0752252c4c13ddc439561bbae
beratukiqi <berat.u@live.se>
2024-03-18 14:22:25 +0100

    Basic route protection

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,16 @@
 import DashboardLayout from "@/components/DashboardLayout";
 import NewClientForm from "@/components/NewClientForm";
 import ClientTableSSR from "@/components/ClientTableSSR";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-  const session = await getServerSession();
-
-  const redirectIfNotSignedIn = async () => {
-    "use server";
-    if (!session) {
-      redirect("/api/auth/signin");
-    }
-  };
   return (
     <DashboardLayout>
-      {session ? (
-        <>
-          <h1 className="text-5xl font-bold my-20 mb-40">Your Dashboard</h1>
-          <ClientTableSSR />
-          <NewClientForm />
-        </>
-      ) : (
-        <form action={redirectIfNotSignedIn}>
-          <h1>Welcome to the dashboard, please sign in to continue</h1>
-          <button type="submit">Sign in</button>
-        </form>
-      )}
+      <>
+        <h1 className="text-5xl font-bold my-20 mb-40">Your Dashboard</h1>
+        <ClientTableSSR />
+        <NewClientForm />
+      </>
+
       {/* <TableHandler type="client" /> */}
     </DashboardLayout>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import NewClientForm from "@/components/NewClientForm";
-import SignInButton from "@/components/LoginNavBar";
-import { getServerSession } from "next-auth";
-import AuthSignIn from "./auth/signin/page";
-import { redirect } from "next/navigation";
-import { getPhotographer } from "./actions";
-
 export default async function Home() {
-  // const session = await getServerSession();
-
-  // if (!session) {
-  //   return <AuthSignIn />;
-  // }
-
-  // redirect("/dashboard");
   return <h1 className="text-7xl">HOME PAGE</h1>;
 }
