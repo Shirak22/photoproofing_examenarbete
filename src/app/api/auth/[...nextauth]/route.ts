e347df15c90bcf2e6e8408e48f8c09f0891158b4
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2024-03-24 22:39:43 +0100

    Deploy Fix remove handler in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -88,6 +88,6 @@ import NextAuth from "next-auth/next";
   
 // };
 
-export const handler = NextAuth(authOptions);
+export default  NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
