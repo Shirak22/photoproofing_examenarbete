cbb3a052c9c4aae42213fe9f805d47f1d7d87638
beratukiqi <berat.u@live.se>
2024-03-27 11:00:15 +0100

    #97 client footer completed

--- src/app/client/[albumId]/page.tsx
+++ src/app/client/[albumId]/page.tsx
@@ -1,4 +1,11 @@
-import { getAlbum, getAlbumThumbnails } from "@/app/actions";
+import {
+  getAlbum,
+  getAlbumThumbnails,
+  getClient,
+  getPhotographer,
+  getPhotographerById,
+} from "@/app/actions";
+import Footer from "@/components/client-gallery/Footer";
 import GallerySection from "@/components/client-gallery/GallerySection";
 import Hero from "@/components/client-gallery/Hero";
 import { getServerSession } from "next-auth";
@@ -10,6 +17,8 @@ export default async function ClientGallery({
   params: { albumId: string };
 }) {
   const album = await getAlbum(params.albumId);
+  const client = await getClient(album.clientId);
+  const photographer = await getPhotographerById(client.photographerId);
 
   if (!album) return notFound();
 
@@ -21,6 +30,7 @@ export default async function ClientGallery({
     <div>
       <Hero title={album.title} description={album.description} />
       <GallerySection album={album} />
+      <Footer photographer={photographer} />
     </div>
   );
 }
